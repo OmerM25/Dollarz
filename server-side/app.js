4d66af73b53532e1c1163caa0a20523fd5d62a49
Omer Moran <imomerm@gmail.com>
2021-01-07 14:25:59 +0200

    Added mongoose & db connection

--- server-side/app.js
+++ server-side/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var mongoose = require('mongoose')
 
 var app = express();
+var mongoDB = "mongodb+srv://dollarz:dollarz123@omerm-cluster.pzgxz.mongodb.net/dollarz?retryWrites=true&w=majority";
 
 app.get('/', (req, res) =>
   res.send('Hello World!')
@@ -10,6 +12,9 @@ app.get('/', (req, res) =>
 const server = app.listen(3000, () => {
   const { address, port } = server.address();
   console.log(`Listening at http://${address}:${port}`);
+
+  mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+  mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 });
 
 module.exports = app;
