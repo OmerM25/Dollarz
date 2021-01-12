3eb481db66d7266328818b72257514d79e3984ec
Sapir-sh <sapir2167@gmail.com>
2021-01-12 16:50:38 +0200

    basic routing

---
+++ pages/chores.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Chores = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text> המטלות בבית שלנו </Text>
+    </View>
+  );
+};
+
+export default Chores;
