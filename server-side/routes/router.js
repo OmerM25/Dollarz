b474193d3447408d52250fa9f2c8c751342d1f09
Sapir-sh <sapir2167@gmail.com>
2021-01-17 20:03:39 +0200

    register function + db struct change

--- server-side/routes/router.js
+++ server-side/routes/router.js
@@ -4,30 +4,31 @@ var router = express.Router();
 var jwt = require("jsonwebtoken");
 
 // Authenticate using jwt using middleware
-router.use((req, res, next) => {
-  // Exclude login and registration page
-  if (req.originalUrl.toString() !== "/login" && req.originalUrl.toString() !== "/register") {
-    const bearerHeader = req.headers["authorization"];
 
-    if (typeof bearerHeader !== "undefined") {
-      // Get the user token
-      const bearer = bearerHeader.split(" ");
-      const bearerToken = bearer[1];
-      req.token = bearerToken;
+// router.use((req, res, next) => {
+//   // Exclude login and registration page
+//   if (req.originalUrl.toString() !== "/user/login" && req.originalUrl.toString() !== "/user/register") {
+//     const bearerHeader = req.headers["authorization"];
 
-      jwt.verify(req.token, "dollarzJwt", (err, authData) => {
-        if (err) {
-          res.sendStatus(403);
-        }
-      });
-    } else {
-      res.sendStatus(403);
-    }
-  }
+//     if (typeof bearerHeader !== "undefined") {
+//       // Get the user token
+//       const bearer = bearerHeader.split(" ");
+//       const bearerToken = bearer[1];
+//       req.token = bearerToken;
 
-  // Continue to next middleware
-  next();
-});
+//       jwt.verify(req.token, "dollarzJwt", (err, authData) => {
+//         if (err) {
+//           res.sendStatus(403);
+//         }
+//       });
+//     } else {
+//       res.sendStatus(403);
+//     }
+//   }
+
+//   // Continue to next middleware
+//   next();
+// });
 
 // module.exports = router;
 export default router;
--- server-side/src/controllers/userController.js
+++ server-side/src/controllers/userController.js
@@ -1,12 +1,16 @@
 var express = require("express");
 var jwt = require("jsonwebtoken");
+var mongoose = require("mongoose");
 
 var router = express.Router();
+var User = require("../user/user");
+var Parent = require("../parent/parent");
 
 router.post("/login", function (req, res, next) {
   const user = {
-    name: "sapir",
-    email: "12@gmail.com",
+    id: req.body.id,
+    name: req.body.name,
+    password: req.body.password,
   };
 
   // ---ADD HERE CHECK WHETHER THE USER EXIST IN THE DB---
@@ -17,6 +21,28 @@ router.post("/login", function (req, res, next) {
       token,
     });
   });
+});
+
+// Register new user to the DB
+router.post("/register", function (req, res, next) {
+  const user = new User({
+    idNumber: req.body.id,
+    name: req.body.name,
+    password: req.body.password,
+  });
+
+  try {
+    user.save().then(() => {
+      const parent = new Parent({
+        userDetails: user._id,
+        children: [],
+        chores: [],
+      });
+      parent.save().then(res.status(200).send());
+    });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
 });
 
 export default router;
