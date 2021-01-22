714496fee5dc4830cae21ac6e179449584f8a26b
Moranzi <moranzi31@gmail.com>
2021-01-22 17:10:08 +0200

    Login page - only client side

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,13 +1,52 @@
-import React from "react";
-import { StyleSheet, View, Image } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Image, TextInput } from "react-native";
+import { Button } from "../common/Button";
 import { CustomText } from "../common/CustomText";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const img = require("../images/LoginPage.png");
-const Login = () => {
+
+const Login = ({ navigation: { navigate } }) => {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = () => {
+    navigate('MainLayout');
+  }
   return (
     <View style={styles.view}>
-      <CustomText style={styles.welcome}>ברוך הבא ל- Dollarz !</CustomText>
-      <Image source={img} style={styles.img} />
+      <CustomText style={styles.welcome}>
+        ברוך הבא ל- Dollarz !
+        </CustomText>
+      <Image
+        source={img}
+        style={styles.img} />
+      <CustomText
+        style={styles.inputHeadline}>
+        תעודת זהות
+        </CustomText>
+      <TextInput
+        value={id}
+        onChangeText={id => setId(id)}
+        style={styles.input} />
+      <CustomText
+        style={styles.inputHeadline}>
+        סיסמא
+        </CustomText>
+      <TextInput
+        value={password}
+        onChangeText={setPassword}
+        style={styles.input} />
+      <Button
+        title="התחבר"
+        color={'#3A23CD'}
+        onPress={handleLogin}>
+      </Button>
+      <Button
+        title="הורה? הירשם עכשו"
+        color={'#3A23CD'}
+        onPress={()=>  navigate('Register')}>
+      </Button>
     </View>
   );
 };
@@ -27,7 +66,18 @@ const styles = StyleSheet.create({
   img: {
     width: 200,
     height: 178,
-    marginTop: 70
-  }
-
+    marginTop: 70,
+    marginBottom: 40
+  },
+  input: {
+    height: 40,
+    width: 180,
+    marginBottom: 20,
+    backgroundColor: '#CCCCCC',
+    borderRadius: 2
+  },
+  inputHeadline: {
+    fontSize: 18,
+    marginBottom: 15,
+  },
 })
