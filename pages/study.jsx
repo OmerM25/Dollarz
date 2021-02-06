76ae9662372585234f84e5c405cfaa03ee1d029a
Moranzi <moranzi31@gmail.com>
2021-02-06 18:17:29 +0200

    start working on game menu

--- pages/study.jsx
+++ pages/study.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { CustomText } from "../common/CustomText";
 
 const Study = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text> ידע זה כוח </Text>
+    <View style={styles.view}>
+      <CustomText style={styles.headline}> ידע זה כוח </CustomText>
+      <CustomText style={{marginTop: 30}}>איזה כיף שבאת דני,</CustomText>
+      <CustomText>ללמוד על כסף יכול להיות ממש כיף עם</CustomText>
+      <CustomText> המשחקים והלומדות שלנו. </CustomText>
+      <CustomText> אז למה אתה מחכה? בוא נתחיל לשחק </CustomText>
+
+
+
     </View>
   );
 };
 
 export default Study;
 
+const styles = StyleSheet.create({
+  view: {
+    alignItems: "center",
+    marginTop: 70,
+  },
+  headline: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color:'#4525F2'
+  },
+});
