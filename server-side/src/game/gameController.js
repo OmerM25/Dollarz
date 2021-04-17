795f61f1d0535bf83bb8bfc39bfb00221fd07cd5
Omer Moran <imomerm@gmail.com>
2021-04-17 21:54:00 +0300

    Finished the image-slider game

--- server-side/src/game/gameController.js
+++ server-side/src/game/gameController.js
@@ -6,6 +6,18 @@ var jwt = require("jsonwebtoken");
 var router = express.Router();
 const fs = require('fs');
 
+
+// Get slider questions game
+router.get("/slider", function (req, res) {
+    try {
+        let gameData = JSON.parse(fs.readFileSync('slider_game.json', 'utf-8'));
+        gameData = shuffleArray(gameData)
+        res.status(200).send(gameData)
+    } catch (ex) {
+        res.status(500).send(ex)
+    }
+})
+
 // Get american questions game
 router.get("/quiz", function (req, res) {
     try {
