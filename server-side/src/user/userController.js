fe2c0df4987d2fa66b67064374ac12c2f22f3836
Sapir-sh <sapir2167@gmail.com>
2021-05-29 18:16:34 +0300

    fix bugs

--- server-side/src/user/userController.js
+++ server-side/src/user/userController.js
@@ -60,7 +60,7 @@ router.post("/registerParent", function (req, res, next) {
     name: req.body.name,
     password: req.body.password,
     lastLogin: new Date(),
-  });
+  }); //
 
   user
     .save()
