b57954ad3db2821f325ff2c1866b8a6ae2c2a67c
Sapir-sh <sapir2167@gmail.com>
2021-05-30 22:35:32 +0300

    ignore warnings

--- App.js
+++ App.js
@@ -3,10 +3,13 @@ import React from "react";
 import { View } from "react-native";
 import FlashMessage from "react-native-flash-message";
 import NavContainer from "./pages/components/NavContainer";
+import { LogBox } from "react-native";
 
+// Ignore log notification by message
+LogBox.ignoreLogs([
+  "VirtualizedLists should never be nested inside plain ScrollViews with the same orientation - use another VirtualizedList-backed container instead",
+]);
 export default function App() {
-  const Stack = createStackNavigator();
-
   return (
     <View style={{ flex: 1 }}>
       <NavContainer />
